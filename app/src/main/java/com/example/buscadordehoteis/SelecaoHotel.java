f3d67e06f1039315efbc6e9c08b19e1e4e590ac9
zebulgado <othon.negromonte@gmail.com>
2020-11-10 11:51:33 -0300

    Inicial

---
+++ app/src/main/java/com/example/buscadordehoteis/SelecaoHotel.java
@@ -0,0 +1,14 @@
+package com.example.buscadordehoteis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelecaoHotel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_hotel);
+    }
+}
---
+++ app/src/main/java/com/example/buscadordehoteis/TelaPrincipal.java
@@ -0,0 +1,18 @@
+package com.example.buscadordehoteis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.buscadordehoteis.R;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+
+    }
+}
