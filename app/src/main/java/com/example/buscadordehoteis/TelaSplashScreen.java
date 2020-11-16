28d91df95b69f8cee92c93d705d7a67cc3d20852
zebulgado <othon.negromonte@gmail.com>
2020-11-16 15:57:28 -0300

    TelaCadastro adjusted.

--- app/src/main/java/com/example/buscadordehoteis/TelaSplashScreen.java
+++ app/src/main/java/com/example/buscadordehoteis/TelaSplashScreen.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.buscadordehoteis.view.TelaPrincipal;
+
 public class TelaSplashScreen extends AppCompatActivity {
 
     Button btButton;
--- app/src/main/java/com/example/buscadordehoteis/view/TelaCadastro.java
+++ app/src/main/java/com/example/buscadordehoteis/view/TelaCadastro.java
@@ -3,14 +3,22 @@ package com.example.buscadordehoteis.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.buscadordehoteis.R;
+import com.example.buscadordehoteis.service.MaskEditUtil;
 
 public class TelaCadastro extends AppCompatActivity {
+
+    EditText edNascimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+
+        edNascimento = findViewById(R.id.ed_nascimento);
+
+        edNascimento.addTextChangedListener(MaskEditUtil.mask(edNascimento, "##/##/####"));
     }
 }
