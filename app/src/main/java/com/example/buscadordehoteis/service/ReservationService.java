bcd056d713dcd55c3f084cf0d71853c2a734b267
zebulgado <othon.negromonte@gmail.com>
2020-11-23 16:22:10 -0300

    Reservation done

--- app/src/main/java/com/example/buscadordehoteis/service/ReservationService.java
+++ app/src/main/java/com/example/buscadordehoteis/service/ReservationService.java
@@ -1,25 +1,32 @@
 package com.example.buscadordehoteis.service;
 
+import com.example.buscadordehoteis.model.Guest;
 import com.example.buscadordehoteis.model.Reservation;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ReservationService {
 
     @GET("reservation")
     Call<List<Reservation>> getAllReservation();
 
-    @GET("reservation")
-    Call<Reservation> getReservation(Long id);
+    @GET("reservation/{id}")
+    Call<Reservation> getReservation(@Path("id") Long id);
 
     @POST("reservation")
-    void insert();
+    Call<Reservation> insertReservation(@Body Reservation reservation);
 
-    @DELETE("reservation")
-    void delete();
+    @PUT("reservation")
+    Call<Guest> updateReservation(@Body Reservation reservation);
+
+    @DELETE("guest/{id}")
+    Call<Void> deleteReservation(@Path("id") Long id);
 }
